4f2f1f4211f3e1a273e984dbb04223692af6b19e
bemar24 <joa.berriel.24@lehre.mosbach.dhbw.de>
2025-02-05 20:55:52 +0100

    Services/ Treatment and Workshops

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,1 +1,14 @@
-import nav
+fetch('../components/navbar/navbar.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('navbar').innerHTML = data)
+    .catch(error => console.error('Error loading navbar:', error));
+
+<!--function for open and close menubar-->
+function showMenubar() {
+    const menubar = document.querySelector('.menubar')
+    menubar.style.display = 'flex'
+}
+function closeMenubar(){
+    const menubar = document.querySelector('.menubar' )
+    menubar.style.display = 'none'
+}
