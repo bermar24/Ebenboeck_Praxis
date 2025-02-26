27f5d90ce26c3f1ed6de55c4176d69ffe426366c
fjkun06 <fjkun06@gmail.com>
2025-02-27 00:37:43 +0100

    refactored footer

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,4 +1,7 @@
-fetch('/frontend/src/components/footer/footer.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('footer').innerHTML = data)
-    .catch(error => console.error('Error loading footer:', error));
+const insertFooter = () =>
+  fetch("/frontend/src/components/footer/footer.html")
+    .then((response) => response.text())
+    .then((data) => (document.getElementById("footer").innerHTML = data))
+    .catch((error) => console.error("Error loading footer:", error));
+
+    insertFooter();
