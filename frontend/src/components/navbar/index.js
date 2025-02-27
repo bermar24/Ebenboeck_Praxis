fc8c6e24b99fdd18bd4d2a95c8e565127faf1a2e
fjkun06 <fjkun06@gmail.com>
2025-02-27 03:44:28 +0100

    Completed responsiveness

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,21 +1,21 @@
+//adding navbar and css styles dynamically
+fetch("../components/layout/layout.html")
+  .then((response) => response.text())
+  .then((data) => (document.querySelector("head").innerHTML += data))
+  .catch((error) => console.error("Error loading navbar:", error));
+
 fetch("../components/navbar/navbar.html")
   .then((response) => response.text())
   .then((data) => (document.querySelector("header").innerHTML = data))
   .catch((error) => console.error("Error loading navbar:", error));
 
-//adding navbar and css styles dynamically
-fetch("../components/layout/layout.html")
-  .then((response) => response.text())
-  .then((data) => (document.querySelector("header").innerHTML += data))
-  .catch((error) => console.error("Error loading navbar:", error));
-
 function showMenubar() {
   const menubar = document.querySelector(".menubar");
   menubar.style.display = "flex";
-//   menubar.classList.toggle("slide-in");
+  //   menubar.classList.toggle("slide-in");
 }
 function closeMenubar() {
   const menubar = document.querySelector(".menubar");
   menubar.style.display = "none";
-//   menubar.classList.toggle("slide-in");
+  //   menubar.classList.toggle("slide-in");
 }
