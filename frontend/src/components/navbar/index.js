be512240c62e66fa1e6fc7014f9c0321edf0be1c
fjkun06 <fjkun06@gmail.com>
2025-02-27 03:06:45 +0100

    Completed navbar

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,14 +1,21 @@
-fetch('../components/navbar/navbar.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('navbar').innerHTML = data)
-    .catch(error => console.error('Error loading navbar:', error));
+fetch("../components/navbar/navbar.html")
+  .then((response) => response.text())
+  .then((data) => (document.querySelector("header").innerHTML = data))
+  .catch((error) => console.error("Error loading navbar:", error));
 
-//function for open and close menubar
+//adding navbar and css styles dynamically
+fetch("../components/layout/layout.html")
+  .then((response) => response.text())
+  .then((data) => (document.querySelector("header").innerHTML += data))
+  .catch((error) => console.error("Error loading navbar:", error));
+
 function showMenubar() {
-    const menubar = document.querySelector('.menubar')
-    menubar.style.display = 'flex'
+  const menubar = document.querySelector(".menubar");
+  menubar.style.display = "flex";
+//   menubar.classList.toggle("slide-in");
 }
-function closeMenubar(){
-    const menubar = document.querySelector('.menubar' )
-    menubar.style.display = 'none'
+function closeMenubar() {
+  const menubar = document.querySelector(".menubar");
+  menubar.style.display = "none";
+//   menubar.classList.toggle("slide-in");
 }
